72eeaad4057b17c7897b04f568e8fe3eb63a8d6e
Yoeri Van Wassenhove <yoerivw@hotmail.com>
2019-09-28 18:50:45 +0200

    Dom manipulation attempts

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-console.log('learning all these fancy things');
+let appIdeas = [];
+console.log('this is working');
+
+function addIdea() {
+    console.log('this is executed');
+    let idea = document.forms["addIdea"]["appEntry"].value;
+    console.log(idea);
+    let newEl = document.createElement('li');
+
+    let newIdea = document.createTextNode('lol');
+    console.log(newIdea);
+    newEl.appendChild(newIdea);
+
+    let position = document.getElementById('list-parent')[0];
+
+    position.appendChild(newEl);
+    appIdeas.push(idea);
+
+
+
+}
+window.onload = function() {
+    document.getElementById("btn").addEventListener("click", addIdea)
+}
