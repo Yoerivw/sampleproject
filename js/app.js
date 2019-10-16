ad947489028e5e73b7d7fd2bb0547192810c40e3
Yoeri Van Wassenhove <yoerivw@hotmail.com>
2019-10-16 21:57:20 +0200

    new features

--- js/app.js
+++ js/app.js
@@ -2,40 +2,35 @@ let appIdeas = [];
 let elForm = document.getElementById('formSignup');
 let idea = document.getElementById('idea').value;
 
-console.log('this is working');
-
-
-
-
-
-
 
 function addIdea(e, idea) {
     let input = this.idea.value;
-    console.log("this function is being executed");
-    console.log(input);
-
 
     if (!input) {
         alert("please fill out something relevant");
         e.preventDefault();
     } else {
         input = this.idea.value;
-        console.log('input' + input);
         e.preventDefault();
+        //create a li and a element and create text node with input
         let newEl = document.createElement('li');
-        console.log('newEl' + newEl);
+        let newLink = document.createElement('a');
         let newIdea = document.createTextNode(input);
-        console.log('newIdea' + newIdea);
-        newEl.appendChild(newIdea);
+
+        //change attritube of link 
+        newLink.setAttribute('href', 'deleteItem');
+
+        //append the text node to the link
+        newLink.appendChild(newIdea);
+        newEl.appendChild(newLink);
 
         let position = document.getElementsByTagName('ul')[0];
 
         position.appendChild(newEl);
-
-
     }
+}
 
+function deleteIdea(e) {
 
 }
 
